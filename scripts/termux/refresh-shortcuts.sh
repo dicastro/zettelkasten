d7971a987438913223fc7eecb7bb7e0a955dc700
dicastro <diegocastroviadero19@gmail.com>
2024-10-10 16:51:22 +0200

    cambios desde tws-thinkpad

---
+++ scripts/termux/refresh-shortcuts.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && pwd)"
+
+SHORTCUTS_DIR="${HOME}/.shortcuts"
+mkdir -p "$SHORTCUTS_DIR"
+
+cat << EOF > "${SHORTCUTS_DIR}/logseq-push.sh"
+#!/bin/bash
+$SCRIPT_DIR/push.sh
+EOF
+chmod +x "${SHORTCUTS_DIR}/logseq-push.sh"
+
+cat << EOF > "${SHORTCUTS_DIR}/logseq-pull.sh"
+#!/bin/bash
+$SCRIPT_DIR/pull.sh
+EOF
+chmod +x "${SHORTCUTS_DIR}/logseq-pull.sh"
+
+cat << EOF > "${SHORTCUTS_DIR}/logseq-log.sh"
+#!/bin/bash
+$SCRIPT_DIR/log.sh
+EOF
+chmod +x "${SHORTCUTS_DIR}/logseq-log.sh"
